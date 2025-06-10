0af8adeb431e47874d268c89683e81253baeea3e
aryatama2004 <aryatama261@gmail.com>
2025-06-10 15:03:58 +0700

    memberi getline pada tampilan baris dan mengakhiri file

--- iofile.cpp
+++ iofile.cpp
@@ -35,7 +35,14 @@ int main(){
     // jika file ada maka
     if (infile.is_open())
     {
-
+        //melakukan perulangan setiap baris
+        while (getline(infile,baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
     }
   
 }
