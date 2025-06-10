3c925dbe81536014878655cb4940d5c15e981432
aryatama2004 <aryatama261@gmail.com>
2025-06-10 15:34:43 +0700

    menampilkan cout baris

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -41,7 +41,10 @@ int main(){
     {
         //melakukan perulangan setiap baris
         while (getline(infile,baris))
-     
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
     
     }
  
