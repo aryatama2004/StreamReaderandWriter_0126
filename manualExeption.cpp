77fe22aaa5332aa61da9a48bbf186b51d82d41a0
aryatama2004 <aryatama261@gmail.com>
2025-06-10 15:16:13 +0700

    mengawali objek array yang akan di gunakan

---
+++ manualExeption.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+//untuk objek exeption yang akan di gunakaan
+#include <array>
+//untuk objek array yang akan di gunakan
+using namespace std;
+
+int main (){
+
+
+ 
+}
