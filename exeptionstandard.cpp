1f5a3084af60cb9c75cac8ba24d44a67d393d6ac
aryatama2004 <aryatama261@gmail.com>
2025-06-10 15:10:53 +0700

    membuat awal program menggunkan exception dan array pada file

---
+++ exeptionstandard.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <exception>
+//untuk objek exeption yang akan di gunakaan
+#include <array>
+//untuk objek array yang akan di gunakan
+using namespace std;
+
+int main (){
+    cout << "Awal program" << endl; //penanda 1
+
+
+
+}
