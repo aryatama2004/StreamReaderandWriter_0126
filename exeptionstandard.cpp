e39c8d5cd9ce46e34e1077e46dc64180e8e7d22c
aryatama2004 <aryatama261@gmail.com>
2025-06-10 15:11:16 +0700

    memberikan pesan 3 elemen dan memanggil array

--- exeptionstandard.cpp
+++ exeptionstandard.cpp
@@ -7,7 +7,12 @@ using namespace std;
 
 int main (){
     cout << "Awal program" << endl; //penanda 1
-
+    try {
+        array<int,3> data = {1,2,3};
+        //pesan array 3interger 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
 
 
 }
